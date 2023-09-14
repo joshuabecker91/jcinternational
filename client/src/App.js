f1cc1f134a8bb9f671d5043a7b3029ba22d841df
joshuabecker91 <joshua.becker91@gmail.com>
2023-09-14 10:57:54 -0700

    major update, landing page ready, main page awaiting feedback from client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import AllRoutes from "./router/AllRoutes";
+import ScrollToTop from "./components/ScrollToTop";
+import AOS from "aos";
+import "photoswipe/dist/photoswipe.css";
+import "aos/dist/aos.css";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ScrollToTop />
+      <AllRoutes />
+    </>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/footer/FooterAnimation.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+} from "react-icons/fa";
+
+const SocialShare = [
+  { Social: <FaFacebookF />, link: "https://www.facebook.com/jacob.castanon.9" },
+  { Social: <FaInstagram />, link: "https://www.instagram.com/jacobcastanon" },
+];
+
+const Footer = () => {
+  return (
+    <>
+      <div className="row align-items-center">
+        <div className="col-md-6 my-2">
+          <div className="nav justify-content-center text-center text-md-left justify-content-md-start">
+            {SocialShare.map((val, i) => (
+              <a key={i} href={`${val.link}`} rel="noreferrer" target="_blank">
+                {val.Social}
+              </a>
+            ))}
+          </div>
+          {/* End .nav */}
+        </div>
+        {/* End .col */}
+
+        <div className="col-md-6 my-2 text-center text-md-right">
+          <p>
+            © {new Date().getFullYear()} Copyright{" "}
+            <a
+              href="https://jacobcastanon.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              JCI International{" "}
+            </a>
+            all rights reserved
+          </p>
+        </div>
+        {/* End .col */}
+      </div>
+      {/* End .row */}
+    </>
+  );
+};
+
+export default Footer;
